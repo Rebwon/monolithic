2545f657cea2a95e4f943f72602c130d3f3d0c34
Rebwon <msolo021015@naver.com>
2021-08-15 17:51:09 +0900

    handle event

--- src/main/java/com/rebwon/MonolithicApplication.java
+++ src/main/java/com/rebwon/MonolithicApplication.java
@@ -2,6 +2,8 @@ package com.rebwon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class MonolithicApplication {
@@ -10,4 +12,6 @@ public class MonolithicApplication {
         SpringApplication.run(MonolithicApplication.class, args);
     }
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void handle() {}
 }
