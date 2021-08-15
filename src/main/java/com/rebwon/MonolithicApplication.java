57046eb4c34dc37512c08a972b9b502bc3dc0095
Rebwon <msolo021015@naver.com>
2021-08-15 15:54:39 +0900

    Add sample

--- src/main/java/com/rebwon/MonolithicApplication.java
+++ src/main/java/com/rebwon/MonolithicApplication.java
@@ -2,12 +2,18 @@ package com.rebwon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class MonolithicApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MonolithicApplication.class, args);
+    }
+
+    @EventListener
+    public void sample() {
+
     }
 
 }
